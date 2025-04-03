b278cc28eb6bd4dcd5ffe4100fcee50f4cf41872
charurane <140895715+charurane@users.noreply.github.com>
2025-04-03 17:41:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar Scroll Effect
+window.addEventListener("scroll", function() {
+    let navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
